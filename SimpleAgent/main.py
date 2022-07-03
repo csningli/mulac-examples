807c3f1a7fedef3b5e8b620b0a165c719a6f4f71
NiL <csningli@gmail.com>
2022-07-03 13:08:23 +0800

    SimpleAgent tested.

--- SimpleAgent/main.py
+++ SimpleAgent/main.py
@@ -4,22 +4,22 @@ import os, math, functools
 from mulac import *
 
 class SimpleAgent(Agent) :
-    def __init__(self, id) :
+    def __init__(self, id, mark = '0') :
         super(SimpleAgent, self).__init__(id = id)
+        self.mark = mark
         self.done = False
 
     def process(self, msgs) :
-        result = {"msgs" : []}
+        result = {'msgs' : []}
         if self.done == False :
-            self.logger.log("agent %s done." % self.id)
+            self.logger.log('agent %s done.' % self.id)
             self.done = True
-        elif self.id == "0" :
-            result["msgs"].append(Message(src = self.id, dest = None, content = None))
+        elif self.id == self.mark :
+            result['msgs'].append(Message(src = self.id, dest = None, content = None))
         return result
 
 if __name__ == "__main__" :
-    agents = [SimpleAgent(id = str(i)) for i in range(5)]
+    n = 5
+    agents = [SimpleAgent(id = str(i), mark = str(n - 1)) for i in range(n)]
     monitor = Monitor()
-    print("Time cost: %s" % monitor.run(agents = agents, timeout = 1))
-    combine_logs(logs = [monitor.logger.path] + [agent.logger.path for agent in agents],
-        path = "logs/%s_combined_logs.log" % get_datetime_stamp())
+    print("Time cost: %s" % monitor.run(agents = agents, timeout = 2))
